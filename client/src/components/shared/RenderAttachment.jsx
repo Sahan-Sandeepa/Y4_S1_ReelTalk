4c2f20c0384dd35e7d6c8c324599ccdd6bc39ab3
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2024-09-06 18:17:22 +0530

    update RenderAttachment.jsx

--- client/src/components/shared/RenderAttachment.jsx
+++ client/src/components/shared/RenderAttachment.jsx
@@ -5,34 +5,53 @@ import { transformImage } from "../../libs/Features";
 import { FileOpen as FileOpenIcon } from "@mui/icons-material";
 
 const RenderAttachment = (file, url) => {
-
   const navigate = useNavigate();
 
   const handleClick = (e) => {
     e.preventDefault();
     navigate('/movie-detail', { state: { url } });
-  }
+  };
 
   switch (file) {
     case "video":
-      return <video src={url} preload="none" width={"200px"} controls />;
+      return (
+        <video
+          src={url}
+          preload="none"
+          width="200px"
+          controls
+          style={{
+            borderRadius: "8px",
+            objectFit: "cover",
+          }}
+        />
+      );
 
     case "image":
       return (
         <img
           src={transformImage(url, 200)}
-          alt="Attachement"
-          width={"200px"}
-          height={"150px"}
+          alt="Attachment"
+          width="150px"
+          height="200px"
           style={{
-            objectFit: "contain",
+            objectFit: "fill",
+            borderRadius: "8px",
+            cursor: "pointer",
           }}
           onClick={handleClick}
         />
       );
 
     case "audio":
-      return <audio src={url} preload="none" controls />;
+      return (
+        <audio
+          src={url}
+          preload="none"
+          controls
+          style={{ borderRadius: "8px" }}
+        />
+      );
 
     default:
       return <FileOpenIcon />;
