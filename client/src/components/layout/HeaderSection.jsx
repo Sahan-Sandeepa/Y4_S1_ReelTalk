5ef92ab03a884e036e2e82f57b3368bde8f0fe3c
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2024-10-26 19:26:00 +0530

    update HeaderSection.jsx

--- client/src/components/layout/HeaderSection.jsx
+++ client/src/components/layout/HeaderSection.jsx
@@ -37,7 +37,7 @@ const Layout = ({
             >
                 <Toolbar sx={{ justifyContent: 'space-between' }}>
                     <Typography variant="h6" component="div" sx={{ color: mode === 'dark' ? '#fff' : '#202020' }}>
-                        ReelTalk
+                        FamilyFrame
                     </Typography>
                     <Box>
                         <IconBtn
